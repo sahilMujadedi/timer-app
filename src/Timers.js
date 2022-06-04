31138a80ea7d3345a383ac16fc49c475dd30350e
sahilMujadedi <mujadedisahil@gmail.com>
2022-06-04 15:38:02 -0600

    merged branches and timers can now be added using button. base timer is now done.

--- src/Timers.js
+++ src/Timers.js
@@ -1,17 +1,21 @@
+import { useState } from 'react'
 import Timer from './Timer'
 
 const Timers = () => {
-  let savedTimers = [
+  let [savedTimers, setSavedTimers] = useState([
     {id: 0, seconds: 5, minutes: 0, hours: 0}
-  ]
-
+  ])
+  let [timerList, setTimerList] = useState([savedTimers.map((timer) => {
+    return (<Timer passedSeconds={timer.seconds} passedMinutes={timer.minutes} passedHours={timer.hours} key={timer.id}/>)
+  })])
   
-
+  const addNewTimer = () => {
+    setTimerList(timerList.concat(<Timer passedSeconds={0} passedMinutes={1} passedHours={0} key={timerList.length}/>))
+  }
   return ( 
     <>
-      {savedTimers.map((timer) => (
-        <Timer passedSeconds={timer.seconds} passedMinutes={timer.minutes} passedHours={timer.hours} key={timer.id}/>
-      ))}
+      {timerList}
+      <button onClick={addNewTimer}>New Timer</button>
     </>
   );
 }
