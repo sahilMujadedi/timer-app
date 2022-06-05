f27d86142fcc0fcc7d1486229b2b0a9d8d676dcc
sahilMujadedi <mujadedisahil@gmail.com>
2022-06-04 22:43:52 -0600

    fixed weird rendering system

--- src/Timers.js
+++ src/Timers.js
@@ -2,19 +2,24 @@ import { useState } from 'react'
 import Timer from './Timer'
 
 const Timers = () => {
+  let [id, setId] = useState(0)
   let [savedTimers, setSavedTimers] = useState([
-    {id: 0, seconds: 5, minutes: 0, hours: 0}
+    {id: id, seconds: 5, minutes: 0, hours: 0}
   ])
-  let [timerList, setTimerList] = useState([savedTimers.map((timer) => {
-    return (<Timer passedSeconds={timer.seconds} passedMinutes={timer.minutes} passedHours={timer.hours} key={timer.id}/>)
-  })])
   
   const addNewTimer = () => {
-    setTimerList(timerList.concat(<Timer passedSeconds={0} passedMinutes={1} passedHours={0} key={timerList.length}/>))
+    id++
+    setId(id)
+    savedTimers.push({id: id, seconds: 5, minutes: 0, hours: 0})
+    setSavedTimers(savedTimers)
   }
   return ( 
     <>
-      {timerList}
+      {
+        savedTimers.map((timer) => {
+          return (<Timer passedSeconds={timer.seconds} passedMinutes={timer.minutes} passedHours={timer.hours} id={timer.id} key={timer.id}/>)
+        })
+      }
       <button onClick={addNewTimer}>New Timer</button>
     </>
   );
