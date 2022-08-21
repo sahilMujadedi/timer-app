5f44bc2abdb3b3aa373192d3c58f574216e3d1cf
sahilMujadedi <mujadedisahil@gmail.com>
2022-08-21 13:56:04 -0600

    added responsiveness

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="navbar">
       <ul className="navbar-items">
         <li className="navbar-item title">timer-app</li>
-        <li className="navbar-item navbar-link"><a href='https://github.com/sahilMujadedi/timer-app' target='_blank' title='Opens in a new tab'>GitHub</a></li>
+        <li className="navbar-item navbar-link"><a href='https://github.com/sahilMujadedi/timer-app' target='_blank' title='Opens in a New Tab'>GitHub</a></li>
         <li className="navbar-item navbar-link">About</li>
         <li className="navbar-item navbar-link">Contact</li>
       </ul>
