e478cdaf744a60a63f22ad71ce13864599279cbe
Sahil Mujadedi <sahilmujadedi@pop-os.localdomain>
2022-12-15 02:53:49 -0700

    removed contact li and linked about li.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,8 +6,7 @@ const Navbar = () => {
       <ul className="navbar-items">
         <li className="navbar-item title">timer-app</li>
         <li className="navbar-item navbar-link"><a href='https://github.com/sahilMujadedi/timer-app' target='_blank' title='Opens in a New Tab'>GitHub</a></li>
-        <li className="navbar-item navbar-link">About</li>
-        <li className="navbar-item navbar-link">Contact</li>
+        <li className="navbar-item navbar-link"><a href='https://sahilm.tk' target='_blank' title='Opens in a New Tab'>About</a></li>
       </ul>
     </div>
   );
